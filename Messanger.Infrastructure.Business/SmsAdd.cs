2b0de40981a7cdee59b6fb6d577ab31a5ef737df
Desken-van <79146520+Desken-van@users.noreply.github.com>
2021-03-16 21:50:51 +0200

    Add files via upload

---
+++ Messanger.Infrastructure.Business/SmsAdd.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Messanger.Domain.Core;
+using Messanger.Services.Interfaces;
+using Messanger.Infrastructure.Data;
+
+namespace Messanger.Infrastructure.Business
+{
+    public class SmsAdd : IAddSms
+    {
+        UsersContext db;
+        public SmsAdd(UsersContext context)
+        {
+            db = context;
+        }
+        public MessageEntity AddSms(AccountEntity sender, string text, AccountEntity recepient)
+        {
+            MessageEntity counter = db.Sms.OrderByDescending(x => x.Number).FirstOrDefault();
+            int count = 1;
+            if (counter.Number != 0)
+            {
+                count = 1 + counter.Number;
+            }
+            MessageEntity sms = new MessageEntity
+            {
+                Sender = sender.Id,
+                Sms = text,
+                Recipient = recepient.Id,
+                Number = count
+            };
+            return sms;
+        }
+    }
+}
---
+++ Messanger.Infrastructure.Business/UserAdd.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Messanger.Domain.Core;
+using Messanger.Services.Interfaces;
+
+namespace Messanger.Infrastructure.Business
+{
+    public class UserAdd :IAddUser
+    {
+        public AccountEntity AddUser(string username,string password)
+        {
+            AccountEntity user = new AccountEntity
+            {
+                Login = username,
+                Password = password,
+                Role = "User",
+                Status = "Active"
+            };
+            return user;
+        }
+    }
+}
