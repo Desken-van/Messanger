33e5a23c27adcf71592d01c5edd82c8f4fef0764
Desken-van <79146520+Desken-van@users.noreply.github.com>
2021-03-16 21:48:45 +0200

    Add files via upload

---
+++ Messanger.Domain.Interfaces/IAccountRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Messanger.Domain.Core;
+
+namespace Messanger.Domain.Interfaces
+{
+    public interface IAccountRepository : IDisposable
+    {
+        IEnumerable<AccountEntity> GetAccountList();
+        AccountEntity CheckAccount(string username);
+        AccountEntity GetAccount(string username,string password);
+        void Create(AccountEntity item);
+        void Update(AccountEntity item);
+        void Delete(int id);
+        void Save();
+    }
+}
---
+++ Messanger.Domain.Interfaces/IMessageRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Messanger.Domain.Core;
+namespace Messanger.Domain.Interfaces
+{
+    public interface IMessageRepository:IDisposable
+    {
+        IEnumerable<MessageEntity> GetAccountList();
+        MessageEntity CheckMessage(int sender);
+        MessageEntity GetMessage(int sender, int recepient);
+        void Create(MessageEntity item);
+        void Update(MessageEntity item);
+        void Delete(int id);
+        void Save();
+    }
+}
