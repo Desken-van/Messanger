df4303b35dca11b699bdec4ba5595ed0d9ec742a
Desken-van <79146520+Desken-van@users.noreply.github.com>
2021-03-16 21:53:53 +0200

    Add files via upload

---
+++ Messanger.Infrastructure.Data/AccountRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Messanger.Domain.Core;
+using Messanger.Domain.Interfaces;
+
+
+namespace Messanger.Infrastructure.Data
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private UsersContext db;
+        public AccountRepository(UsersContext context)
+        {
+            db = context;
+        }
+
+        public IEnumerable<AccountEntity> GetAccountList()
+        {
+            return db.Logins.ToArray();
+        }
+
+        public AccountEntity CheckAccount(string username)
+        {
+            AccountEntity user = db.Logins.FirstOrDefault(x => x.Login == username);
+            return user;
+        }
+        public AccountEntity GetAccount(string username,string password)
+        {
+            AccountEntity user = db.Logins.FirstOrDefault(x => x.Login == username && x.Password == password);
+            return user;
+        }
+        public void Create(AccountEntity book)
+        {
+            db.Logins.Add(book);
+        }
+
+        public void Update(AccountEntity book)
+        {
+            db.Entry(book).State = (Microsoft.EntityFrameworkCore.EntityState)EntityState.Modified;
+        }
+
+        public void Delete(int id)
+        {
+            AccountEntity book = db.Logins.Find(id);
+            if (book != null)
+                db.Logins.Remove(book);
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Messanger.Infrastructure.Data/MessageRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Messanger.Domain.Core;
+using Messanger.Domain.Interfaces;
+
+
+namespace Messanger.Infrastructure.Data
+{
+    public class MessageRepository : IMessageRepository
+    {
+        private UsersContext db;
+        public MessageRepository(UsersContext context)
+        {
+            db = context;
+        }
+
+        public IEnumerable<MessageEntity> GetAccountList()
+        {
+            return db.Sms.ToArray();
+        }
+
+        public MessageEntity CheckMessage(int sender)
+        {
+            MessageEntity sms = db.Sms.FirstOrDefault(x => x.Sender == sender);
+            return sms;
+        }
+        public MessageEntity GetMessage(int sender,int recepient)
+        {
+            MessageEntity sms = db.Sms.FirstOrDefault(x => x.Sender == sender && x.Recipient == recepient);
+            return sms;
+        }
+        public void Create(MessageEntity item)
+        {
+            db.Sms.Add(item);
+        }
+
+        public void Update(MessageEntity item)
+        {
+            db.Entry(item).State = (Microsoft.EntityFrameworkCore.EntityState)EntityState.Modified;
+        }
+
+        public void Delete(int id)
+        {
+            MessageEntity sms = db.Sms.Find(id);
+            if (sms != null)
+                db.Sms.Remove(sms);
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
