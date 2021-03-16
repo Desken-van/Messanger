d598b2bfc03fe0edb6a4833e7a57cccbcae0328e
Desken-van <79146520+Desken-van@users.noreply.github.com>
2021-03-16 21:56:11 +0200

    Add files via upload

---
+++ Messanger.Services.Interfaces/IAddSms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Messanger.Domain.Core;
+
+namespace Messanger.Services.Interfaces
+{
+    public interface IAddSms
+    {
+        MessageEntity AddSms(AccountEntity sender,string text,AccountEntity recepient);              
+    }
+}
---
+++ Messanger.Services.Interfaces/IAddUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Messanger.Domain.Core;
+
+namespace Messanger.Services.Interfaces
+{
+    public interface IAddUser
+    {
+        AccountEntity AddUser(string username,string password);              
+    }
+}
