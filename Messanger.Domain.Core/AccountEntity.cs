451f4243bbd1a074a78fb82a8d36f37d07c8cd36
Desken-van <79146520+Desken-van@users.noreply.github.com>
2021-03-16 22:29:46 +0200

    Add files via upload

---
+++ Messanger.Domain.Core/AccountEntity.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Messanger.Domain.Core
+{
+    public class AccountEntity
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ Messanger.Domain.Core/MessageEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messanger.Domain.Core
+{
+    public class MessageEntity
+    {
+        public int Id { get; set; }
+        public int Sender { get; set; }
+        public string Sms { get; set; }
+        public int Recipient { get; set; }
+        public int Number { get; set; }
+    }
+}
