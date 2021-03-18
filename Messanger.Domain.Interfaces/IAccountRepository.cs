658fc13f3f35201afc4d9d2aa38bd7cd295634a5
Desken-van <79146520+Desken-van@users.noreply.github.com>
2021-03-18 13:59:18 +0200

    Add files via upload

--- Messanger.Domain.Interfaces/IAccountRepository.cs
+++ Messanger.Domain.Interfaces/IAccountRepository.cs
@@ -1,23 +1,20 @@
-﻿using Microsoft.Analytics.Interfaces;
-using Microsoft.Analytics.Interfaces.Streaming;
-using Microsoft.Analytics.Types.Sql;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using Messanger.Domain.Core;
+using System.Threading.Tasks;
 
 namespace Messanger.Domain.Interfaces
 {
     public interface IAccountRepository : IDisposable
     {
-        IEnumerable<AccountEntity> GetAccountList();
-        AccountEntity CheckAccount(string username);
-        AccountEntity GetAccount(string username,string password);
-        void Create(AccountEntity item);
-        void Update(AccountEntity item);
-        void Delete(int id);
-        void Save();
+        Task<AccountEntity[]> GetAccountList();
+        Task<AccountEntity> CheckAccount(string username);
+        Task<AccountEntity> GetAccount(string username,string password);
+        Task Create(AccountEntity item);
+        Task Update(AccountEntity item);
+        Task Delete(int id);
     }
 }
--- Messanger.Domain.Interfaces/IMessageRepository.cs
+++ Messanger.Domain.Interfaces/IMessageRepository.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
 using Messanger.Domain.Core;
 namespace Messanger.Domain.Interfaces
 {
     public interface IMessageRepository:IDisposable
     {
-        IEnumerable<MessageEntity> GetAccountList();
-        MessageEntity CheckMessage(int sender);
-        MessageEntity GetMessage(int sender, int recepient);
-        void Create(MessageEntity item);
-        void Update(MessageEntity item);
-        void Delete(int id);
-        void Save();
+        Task<MessageEntity[]> GetMessageList();
+        Task<MessageEntity> CheckMessage(int sender);
+        Task<MessageEntity> GetMessage(int sender, int recepient);
+        Task Create(MessageEntity item);
+        Task Update(MessageEntity item);
+        Task Delete(int id);
     }
 }
