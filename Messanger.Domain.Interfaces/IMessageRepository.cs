51bdc3d8798cdadedc3bcd3b6f440def0d924426
Desken-van <79146520+Desken-van@users.noreply.github.com>
2021-03-18 20:40:41 +0200

    Add files via upload

--- Messanger.Domain.Interfaces/IMessageRepository.cs
+++ Messanger.Domain.Interfaces/IMessageRepository.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Messanger.Domain.Core;
 namespace Messanger.Domain.Interfaces
 {
     public interface IMessageRepository:IDisposable
     {
-        Task<MessageEntity[]> GetMessageList();
-        Task<MessageEntity> CheckMessage(int sender);
-        Task<MessageEntity> GetMessage(int sender, int recepient);
+        Task<IQueryable<MessageEntity>> GetMessageList();
+        Task<MessageEntity> CheckMessage(string sender);
+        Task<MessageEntity> GetMessage(string sender, string recepient);
         Task Create(MessageEntity item);
         Task Update(MessageEntity item);
         Task Delete(int id);
